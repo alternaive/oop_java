e1d803ec933eda41c9a822626e7c351f306d13f9
alternaive <ullavolke@gmail.com>
2018-11-29 09:45:24 +0200

    Essad andmed

---
+++ kapsel/src/Print.java
@@ -0,0 +1,18 @@
+public class Printer {
+
+    private Integer tooneriTase;
+    private Integer paberPrintimiseks;
+    private Boolean kahepoolne;
+
+    public Printer (Integer tooneriTase, Boolean kahepoolne) {
+        this.tooneriTase = tooneriTase;
+        this.kahepoolne = kahepoolne;
+        this.paberPrintimiseks= 100;
+    }
+//  Meetod
+
+
+    public Integer lisaTooner() {
+        if (tooneriTase > )
+    }
+}
