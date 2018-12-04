a34904e23af9351c229e9e9223c2cd101881e72d
alternaive <ullavolke@gmail.com>
2018-12-04 14:11:19 +0200

    konstruktorid

--- kapsel/src/suva.java
+++ kapsel/src/suva.java
@@ -12,24 +12,3 @@ public class suva {
     }
 }
 
-class Tootaja
-{
-    private String nimi;
-    Tootaja(String nimi){
-        this.nimi = nimi;
-    }
-
-    public String getTootajaNimi() {
-        return this.nimi;
-    }
-}
-
-class SEB
-{
-public static void main (String[] args)
-{
-suva bank = new suva ("SEB");
-Tootaja kertu = new Tootaja ("Kertu");
-        System.out.println(kertu.getTootajaNimi()+"töötab" + bank.getPangaNimi() + "pankas");
-}
-}
